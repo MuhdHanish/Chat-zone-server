9598d67fe3d7efdd087a0e4390025ace8fb9d052
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-18 13:29:03 +0530

    Completed and now going to deploye on heruko

--- server.js
+++ server.js
@@ -27,4 +27,31 @@ app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT 
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}...`.green.underline))
+const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}...`.green.underline))
+const io = require('socket.io')(server, {
+ pingTimeout: 60000,
+ cors: {
+  origin: process.env.CORS_ORIGIN_URL
+ }
+})
+io.on("connection", (socket) => {
+ console.log(`connected to socket.io`);
+ socket.on("setup", (userData) => {
+  socket.join(userData?._id);
+  socket.emit("connected");
+ })
+ socket.on("join chat", (room) => {
+  socket.join(room);
+  console.log(`User joined room: ${room}`)
+ })
+ socket.on("typing", (room) => socket.in(room).emit("typing"));
+ socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+ socket.on("new message", (newMessageRecieved) => {
+  let chat = newMessageRecieved?.chat;
+  if (!chat?.users) return console.log(`chat. user not defined`);
+  chat?.users.forEach((user) => {
+   if (user?._id === newMessageRecieved?.sender?._id) return;
+   socket.in(user?._id).emit("message recieved",newMessageRecieved)
+  })
+ })
+});
